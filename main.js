46221e014b03675533b47fa96754a7cca9271160
Ionica Bizau <bizauionica@yahoo.com>
2013-05-31 16:31:46 +0300

    Added the operations logic: search for mongo id.

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 module.exports = function (config) {
-    // ...
+
+    var self = this;
+
+    // first, scan the url
+    var search = window.location.search;
+    var mongoId = search.substring(9);
+    
+    initialize(self, mongoId, function (err, data) {
+        $(".result").text(data);            
+        stopLoading();
+    });
 };
+
+function initialize (self, mongoId, callback) {
+    // initialize
+    self.link("initialize", { data: mongoId }, callback);
+}
+
+function stopLoading () {
+    $(".loading").fadeOut(function () {
+        $(this).remove();
+    });
+}
--- operations.js
+++ operations.js
@@ -1,5 +1,34 @@
 exports.initialize = function (link) {
-    link.send(200);
+
+    var mongoId = link.data;
+
+    if (!mongoId) { link.send(400, "Missing mongo id."); }
+    
+    /////////////////////////////////
+    // Search in database for mongoId
+    /////////////////////////////////
+    M.datasource.resolve(link.params.ds, function(err, ds) {
+        if (err) { return callback(err); }
+        
+        M.database.open(ds, function(err, db) {
+            if (err) { return callback(err); }
+
+            db.collection(ds.collection, function(err, collection) {
+                if (err) { return callback(err); }
+
+                collection.findOne({"_id":M.mongo.ObjectID(mongoId) }, function(err, doc) {
+
+                    if (err) { return link.send(400, err); }
+                    if (!doc) { return link.send(400, "Invalid mongo id."); }
+                    link.send(200, JSON.stringify(link.session, null, 4));
+
+
+
+                    callback(null, docs[0]);
+                });
+            });
+        });
+    });
 };
 
 exports.saveFile = function (link) {
