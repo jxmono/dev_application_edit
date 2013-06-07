fb12ade6cd6291b537072dc64e863bb454235671
Ionica Bizau <bizauionica@yahoo.com>
2013-06-07 15:08:37 +0300

    Terminal animations.

--- terminal-animations.js
+++ terminal-animations.js
@@ -1,4 +1,50 @@
+M.wrap('github/IonicaBizau/dev_application_edit/dev/terminal-animations.js', function (require, module, exports) {
 $(document).ready(function () {
+
+    var TerminalContainer = $(".terminal-container");
+
     // mouse enter -> show
+    var Terminal = {
+        "selector": ".terminal-container",
+        "size": {
+            "initial": {
+                "w": TerminalContainer.css("width"),
+                "h": TerminalContainer.css("height")
+            },
+            "active": {
+                "w": "100%",
+                "h": "150px"
+            }
+        },
+        "active": false,
+        "moving": false
+    };
+
+    TerminalContainer
+    .on("mouseenter", function () {
+        if (Terminal.active || Terminal.moving) { return; }
+
+        Terminal.moving = true;
+
+        TerminalContainer.animate({"width": Terminal.size.active.w }, function () {
+            TerminalContainer.animate({"height": Terminal.size.active.h }, function () {
+                Terminal.active = true;
+                Terminal.moving = false;
+            });
+        });
+    })
     // mouse leave -> timeout --> hide
+    .on("mouseleave", function () {
+        if (!Terminal.active || Terminal.moving) { return; }
+        Terminal.moving = true;
+
+        TerminalContainer.animate({"height": Terminal.size.initial.h }, function () {
+            TerminalContainer.animate({"width": Terminal.size.initial.w }, function () {
+                Terminal.active = false;
+                Terminal.moving = false;
+            });
+        });
+    });
 });
+
+return module; });
